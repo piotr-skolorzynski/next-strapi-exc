1590c8cee9416f993f23222e1810e17daf7b26c7
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-01-14 13:43:31 +0100

    Frontend Add subscribe functionality to SubscribeToNewsletter component

--- surfcamp_frontend/app/_components/Blog/SubscribeToNewsletter.jsx
+++ surfcamp_frontend/app/_components/Blog/SubscribeToNewsletter.jsx
@@ -1,28 +1,52 @@
 "use client";
+import { useState } from "react";
 
 const SubscribeToNewsletter = () => {
+  const [email, setEmail] = useState("");
+  const [hasSignedUp, setHasSignedUp] = useState(false);
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    if (email.length) {
+      //TODO: send email to strapi
+      setHasSignedUp(true);
+    }
+  };
+
   return (
     <section className="newsletter">
-      <div className="newsletter__info">
-        <h4>subscribe to our newsletter</h4>
-        <p className="copy">
-          Unlock Exclusive Insights and Stay In the Know – Subscribe to Our
-          Newsletter Today to always stay in touch
-        </p>
-      </div>
-      <form className="newsletter__form">
-        <input
-          type="email"
-          className="newsletter__email input"
-          placeholder="Enter your email address"
-        />
-        <button
-          className="newsletter__subscribe btn btn--medium btn--turquoise"
-          type="submit"
-        >
-          SUBSCRIBE
-        </button>
-      </form>
+      {hasSignedUp ? (
+        <h4 className="newsletter__thanks">Thank you for subscribing!</h4>
+      ) : (
+        <>
+          <div className="newsletter__info">
+            <h4>subscribe to our newsletter</h4>
+            <p className="copy">
+              Unlock Exclusive Insights and Stay In the Know – Subscribe to Our
+              Newsletter Today to always stay in touch
+            </p>
+          </div>
+          <form className="newsletter__form" onSubmit={handleSubscribe}>
+            <input
+              type="email"
+              className="newsletter__email input"
+              placeholder="Enter your email address"
+              value={email}
+              onChange={onEmailChange}
+            />
+            <button
+              className="newsletter__subscribe btn btn--medium btn--turquoise"
+              type="submit"
+            >
+              SUBSCRIBE
+            </button>
+          </form>{" "}
+        </>
+      )}
     </section>
   );
 };
